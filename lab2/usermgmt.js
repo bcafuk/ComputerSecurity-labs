4e98328dbc65135afede3c270b3fa5641905522c
Borna Cafuk <borna.cafuk@gmail.com>
2021-04-29 14:10:31 +0200

    Initialize files for second lab

---
+++ lab2/usermgmt.js
@@ -0,0 +1,12 @@
+/*
+ * usermgmt.js
+ *
+ * A user account management tool.
+ *
+ * Made for the second laboratory assignment for the course Computer Security
+ * at the Faculty of Electrical Engineering and Computing
+ * at the University of Zagreb, Croatia in the academic year 2020/2021.
+ *
+ * © 2021 Borna Cafuk, All rights reserved
+ * JMBAG (matriculation number): 0036513396
+ */
